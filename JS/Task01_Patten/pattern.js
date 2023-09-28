71d91975ff5b7b32215a5462fd4558d637a57c8e
Chordify <adharsh.c@chordify.com>
2023-09-28 14:35:47 +0530

    initial upload

---
+++ JS/Task01_Patten/pattern.js
@@ -0,0 +1,28 @@
+
+
+document.write("HEY WHATS UP???  </br>" )
+
+
+
+let a='';
+let rows = 5;
+
+pattern=()=>{
+
+
+    for(i=1;i<=rows;i++){
+        
+       
+        for(j=1;j<=i;j++){
+            a+=i;
+        }
+        a+="</br>";
+        
+    }
+    document.write(a);
+    console.log(a);
+    
+}
+
+
+pattern();
