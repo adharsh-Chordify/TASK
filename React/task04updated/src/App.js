c6ec934735838b8b2f67bac4983ed697b213c994
Chordify <adharsh.c@chordify.com>
2023-10-12 22:46:50 +0530

    React Yup and conditional dropdown

--- React/task04updated/src/App.js
+++ React/task04updated/src/App.js
@@ -1,24 +1,48 @@
 
 import { useForm } from 'react-hook-form';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as yup from "yup"
+import {yupResolver} from "@hookform/resolvers/yup"
+
+const schema=yup.object().shape({
+  namedata:yup.string().required("This field cannot be empty"),
+  agedata:yup.string().max(2).required("This field cannot be empty"),
+  emaildata:yup.string().email().required("This field cannot be empty"),
+  state:yup.string().required("This field cannot be Empty"),
+  district:yup.string().required("This field cannot be Empty")
+
+})
 
 function App() {
+  let States=["Kerala","TamilNadu","Karnataka"]
+  let Districts={
+     "Kerala":["Palakkad","Ernakulam","Thrissur"],
+     "TamilNadu":["Chennai","Coimbatore","Tirupur"],
+     "Karnataka":["Banglore","Kolar"]
+ 
+  }
 
-  const {register,handleSubmit,formState: {errors}}=useForm({
-    defaultValues:{
-      namedata:'',
-      agedata:'',
-      emaildata:''
-    }
+ 
+
+ 
+  const {register,handleSubmit,watch,formState: {errors}}=useForm({
+ resolver:yupResolver(schema)
   })
+  const[district,setDistrict]=useState()
+  const[statevalue,setStatevalue]=useState('')
   const [Name,setName]=useState()
   const [Age,setAge]=useState()
   const [Email,setEmail]=useState()
 
+ const temp=watch("state")
+
 // console.log(errors);
 // console.log(watch());
-
+const handleChange =(e)=>{
+  setStatevalue(e.target.value)
+  console.log(district);
+}
 
   return (
     <div className="App">
@@ -28,28 +52,58 @@ function App() {
         setName(data.namedata);
         setAge(data.agedata);
         setEmail(data.emaildata);
+        setStatevalue(data.state)
+        setDistrict(data.district)
+        console.log(data.district);
         // console.log(formdata.agedata);
         // console.log(formdata.namedata);
       })}  >
         <div className="formback">
           <h2 className="heading">React Form</h2>
           <label>Enter Your Name</label>
-          <input {...register('namedata',{required:"This field cannot be empty"})} type="text" placeholder="Enter the name" id='name'></input> <br></br>
+          <input {...register('namedata')} type="text" placeholder="Enter the name" id='name'></input> <br></br>
              <p className="error" >{errors.namedata?.message}</p>  
          
           
           <label>Enter Your Age</label>
-          <input id='age' type="number"  placeholder="Enter your Age" {...register("agedata",{required:"This field cannot be empty",maxLength:{
-            value:2,message:"Minimum input is 2 Numbers" 
-          }})} ></input><br></br>
+          <input id='age' type="number"  placeholder="Enter your Age" {...register("agedata")} ></input><br></br>
          
             <p className="error" >{errors.agedata?.message}</p>  
          
           <label>Enter Your Email</label>
-          <input id='email' type="email" placeholder="Enter your Email" {...register("emaildata",{required:"This field cannot be empty"})} ></input> <br></br>
-          <p className="error" >{errors.emaildata?.message}</p>  
+          <input id='email' type="email" placeholder="Enter your Email" {...register("emaildata")} ></input> <br></br>
+          <p className="error" >{errors.emaildata?.message}</p>
+           
+          <label>Select Your State</label>
+        <select className='district' {...register("state")}>
+        <option  value={''}>Select Your State</option>
+          {
+            States.map(state=>{
+            
+             return <option>{state}</option>
+            })
+          }
+        </select>
+         
+        <p className="error" >{errors.state?.message}</p>  
+
+        <label>Select Your Districts</label>
+        <select className='district' {...register('district')}>
+          <option value={''}>Select Your District</option>
+           {
+            temp && Districts[temp].map(dist=>{
+              return <option>{dist}</option>
+            })
+           }
+        </select>
+
+        <p className="error" >{errors.district?.message}</p>  
+
           <button className='buttonsub' type="submit"  >Click Here To Display</button>
         </div>
+
+    
+
             
       </form>
   { Name?( 
@@ -58,6 +112,8 @@ function App() {
          <p className='outputp'>The Entered Name is {Name}</p>
          <p className='outputp'>The Entered Age is {Age}</p>
          <p className='outputp'>The Entered Email is {Email}</p>
+         <p className='outputp'>The Selected Location is {district} , {statevalue} </p>
+         
      </div>
 ):''}
 
